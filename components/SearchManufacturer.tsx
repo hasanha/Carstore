fdc9a2e46014ac5a0826ea5075f37ef72cfc13d5
hasanha <hasan.hammadeh7@gmail.com>
2023-08-12 16:20:52 -0500

    updatesearchman

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -45,9 +45,9 @@ const SearchManufacturer = ({
 
           <Transition
             as={Fragment}
-            Leave="transition ease-in duration-100"
-            LeaveFrom="opacity-100"
-            LeaveTo="opacity-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options>
